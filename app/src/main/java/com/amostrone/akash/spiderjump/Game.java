c1fd2fcdc9abc478b8af91990967501ff1aad6e8
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-08-20 20:05:28 +0530

    Added level 1 apk.

--- app/src/main/java/com/amostrone/akash/spiderjump/Game.java
+++ app/src/main/java/com/amostrone/akash/spiderjump/Game.java
@@ -58,7 +58,7 @@ public class Game extends View {
 
         //Background Music
         //TODO start the music
-        //ring_background.start();
+        ring_background.start();
 
         if(player_Y==0)player_Y=height;
         else if(player_Y!=height)player_Y+=2;
