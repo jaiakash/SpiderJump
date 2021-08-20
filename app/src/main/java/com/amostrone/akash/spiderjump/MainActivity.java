8ad8be86dbda1dd0e1ccb44ce46317f3161be949
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-08-20 18:55:36 +0530

    Added canvas.

--- app/src/main/java/com/amostrone/akash/spiderjump/MainActivity.java
+++ app/src/main/java/com/amostrone/akash/spiderjump/MainActivity.java
@@ -2,13 +2,19 @@ package com.amostrone.akash.spiderjump;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Game game;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        game =new Game(this);
+        game.setBackgroundColor(Color.BLACK);
+        setContentView(game);
     }
 }
