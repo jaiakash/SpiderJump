b1e69c76f39e0dbf10bb8b3964d3001308b54d4b
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-08-21 12:48:01 +0530

    Enemy can spawn from random y coordinate.

--- app/src/main/java/com/amostrone/akash/spiderjump/Game.java
+++ app/src/main/java/com/amostrone/akash/spiderjump/Game.java
@@ -37,6 +37,7 @@ public class Game extends View {
     boolean clicked=false;
     int[] drawable_enemy_ship = {R.drawable.ship1, R.drawable.ship2, R.drawable.ship3, R.drawable.ship4, R.drawable.ship5};
     int random_enemy_drawable = ThreadLocalRandom.current().nextInt(0, 4 + 1);
+    int randSpawn = ThreadLocalRandom.current().nextInt(-2,2+1);
 
     MediaPlayer ring_background= MediaPlayer.create(getContext(),R.raw.background);
 
@@ -70,36 +71,17 @@ public class Game extends View {
         if(player_Y==0)player_Y=height/2;
         else if(player_Y<height/2)player_Y+=2;
         else if(player_Y>height/2)player_Y-=2;
-        if(player_Y<height/2-160){
-            player_Y=height/2-160;
+        if(player_Y<height/2-220){
+            player_Y=height/2-220;
             clicked=false;
         }
-        if(player_Y>height/2+160){
-            player_Y=height/2+160;
+        if(player_Y>height/2+190){
+            player_Y=height/2+190;
             clicked=false;
         }
         if(clicked){
             player_Y+=player_jump;
         }
-
-        if(enemy_X<0){
-            enemy_X=getWidth();
-            random_enemy_drawable= ThreadLocalRandom.current().nextInt(0, 4 + 1);
-            score_val+=2;
-            speed_enemy++;
-        }
-        else{
-            enemy_X-=speed_enemy;
-        }
-        paint_enemy.setColor(Color.RED);
-        enemy.bottom=height/2-15;
-        enemy.top=height/2-85;
-        enemy.left=enemy_X-110;
-        enemy.right=enemy_X-40;
-        Drawable enmy = getResources().getDrawable(drawable_enemy_ship[random_enemy_drawable], null);
-        enmy.setBounds(enemy.left, enemy.top, enemy.right, enemy.bottom);
-        enmy.draw(canvas);
-        //canvas.drawRect(enemy,paint_enemy);
 
         paint_player.setColor(Color.GREEN);
         player.bottom=player_Y-15;
@@ -111,6 +93,26 @@ public class Game extends View {
         plyr.draw(canvas);
         //canvas.drawCircle(50, player_Y-40, 40, paint_player);
         //canvas.drawRect(player,paint_player);
+
+        if(enemy_X<0){
+            enemy_X=getWidth();
+            random_enemy_drawable= ThreadLocalRandom.current().nextInt(0, 4 + 1);
+            randSpawn = ThreadLocalRandom.current().nextInt(-80,80+1);
+            score_val+=2;
+            speed_enemy++;
+        }
+        else{
+            enemy_X-=speed_enemy;
+        }
+        paint_enemy.setColor(Color.RED);
+        enemy.bottom=height/2-15+randSpawn;
+        enemy.top=height/2-85+randSpawn;
+        enemy.left=enemy_X-110;
+        enemy.right=enemy_X-40;
+        Drawable enmy = getResources().getDrawable(drawable_enemy_ship[random_enemy_drawable], null);
+        enmy.setBounds(enemy.left, enemy.top, enemy.right, enemy.bottom);
+        enmy.draw(canvas);
+        //canvas.drawRect(enemy,paint_enemy);
 
         paint_score.setColor(Color.RED);
         paint_score.setTextSize(45);
